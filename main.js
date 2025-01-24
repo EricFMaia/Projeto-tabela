4b3d2dbef9466a63a23f9af865bce47a41698d76
Eric Ferreira <ericfmaia1490@gmail.com>
2025-01-24 12:07:36 -0300

    corrigindo e organizando

--- main.js
+++ main.js
@@ -4,24 +4,92 @@ let conter = document.getElementById('conter')
 let btnButton = document.getElementById('btnButton')
 let tableGrid = document.getElementById('tableGrid')
 
-function clickConter() {
-    let num = 0
-    conter.addEventListener('click', (event) => {
-        if (event.target.id === "idBtnDec" && num > 0)
-            num--
-        else if (event.target.id === "idBtnInc")
-            num++
-        numClasses.innerHTML = num
-    });
-}
-clickConter()
+let rawNumclass = 0
 
+
+function initializeTable(){
+
+
+    let rawDataMatrix = [
+            67, 67, 67, 68, 68,
+            68, 68, 72, 73, 74,
+            74, 74, 75, 77, 77,
+            78, 78, 80, 85, 85
+    ];
+
+    //transformando a trabela em formato crescente
+    const dataMatrix = rawDataMatrix.sort()
+
+    // tamanho da tabela
+    let N = rawDataMatrix.length
+
+    // formula de Sturges para obter o K(classe)
+    let sturgesfomule = 1 + 3.3 * Math.log10(N)
+
+    //obtendo o valor minimo da tabela
+    const min = Math.min(...dataMatrix)
+
+    //obtendo o valor maximo da tabela
+    const max = Math.max(...dataMatrix)
+
+    let k = getClass(rawNumclass, sturgesfomule)
+
+    // obtendo a amplitude total subistraindo o valor maximo pelo menor valor
+    let At = max - min
+
+    // tamanho da classe dividindo amplitude total pelo numero de classes
+    let rawAi = At / k
+
+    // arredondando o valor para cima não importa o valor quebrado
+    let Ai = Math.ceil(rawAi)
+
+    const interval = createInterval(min, k, Ai)
+
+    const fi = createFrequency(interval, dataMatrix) 
+
+    const fr = createRelativeFrequency(fi)
+
+    let sum = fr.reduce((acc, current) => {
+        return acc + current;
+    });
+
+
+
+    const Fi = calculateFrequencies(fi)
+
+    const Fr = calculateFrequencies(fr)
+    let contentTable = [interval,fi,Fi,fr,Fr]
+
+    createtable(k,tableGrid,contentTable,sum)
+
+    console.log(k+" --CLASSES--")
+    console.log(interval+" --intervals--")
+    console.log(fi+" --fi--")
+    console.log(Fi+" --Fi--")
+    console.log(fr+" --fr--")
+    console.log(Fr+" --Fr--")
+    console.log(sum+" --sum--")
+    console.log(contentTable)
+    console.log(rawDataMatrix)
+
+
+    
+}
+
+conter.addEventListener('click', (event) => {
+    if (event.target.id === "idBtnDec" && rawNumclass > 0)
+        rawNumclass--
+    else if (event.target.id === "idBtnInc")
+        rawNumclass++
+    numClasses.innerHTML = rawNumclass
+    
+});
 
 function clickCreateButton() {
     let isClickable = true;
     btnButton.addEventListener('click', () => {
         if (isClickable)
-        createtable(k,tableGrid,contentTable,sum)
+        initializeTable()
         isClickable = false;
 
         
@@ -30,43 +98,23 @@ function clickCreateButton() {
 
 clickCreateButton()
 
+function getClass(rawNumclass, sturgesfomule){
 
-let rawDataMatrix = [
-    67, 67, 67, 68, 68,
-    68, 68, 72, 73, 74,
-    74, 74, 75, 77, 77,
-    78, 78, 80, 85, 85
-];
+    if (rawNumclass == 0) {
+        //Arredondando o valor do calculo da formula de Sturges 
+        num = Math.round(sturgesfomule)
+    }else{
+        num = rawNumclass
+        
+    }
+    return num
+}
 
-//transformando a trabela em formato crescente
-const dataMatrix = rawDataMatrix.sort()
 
-// tamanho da tabela
-let N = rawDataMatrix.length
 
-// formula de Sturges para obter o K(classe)
-let sturgesfomule = 1 + 3.3 * Math.log10(N)
-
-//Arredondando o valor do calculo da formula de Sturges 
-let k = Math.round(sturgesfomule)
-
-//obtendo o valor minimo da tabela
-const min = Math.min(...dataMatrix)
-
-//obtendo o valor maximo da tabela
-const max = Math.max(...dataMatrix)
-
-// obtendo a amplitude total subistraindo o valor maximo pelo menor valor
-let At = max - min
-
-// tamanho da classe dividindo amplitude total pelo numero de classes
-let rawAi = At / k
-
-// arredondando o valor para cima não importa o valor quebrado
-let Ai = Math.ceil(rawAi)
 
 // criador de intervalos
-function createInterval(){
+function createInterval(min, k, Ai){
 
     const interList = []
     let interValue = min
@@ -78,24 +126,35 @@ function createInterval(){
     return interList
 }
 
-const interval = createInterval()
+
 
 function createFrequency(list, table) {
-    let intervals = list.map(item => item.split("-"));
+    let intervals = list.map(item => item.split("-").map(Number)); 
     let rawfi = [];
 
-    for( i = 0 ; i < intervals.length ; i++){
-        let acc = 0
+    for (let i = 0; i < intervals.length; i++) {
+        let acc = 0;
         table.forEach((element) => {
-            if(element >= intervals[i][0] && element < intervals[i][1]){
-                acc++
+           
+            if (i === intervals.length - 1) {
+                
+                if (element >= intervals[i][0] && element <= intervals[i][1]) {
+                    acc++;
+                }
+            } else {
+                
+                if (element >= intervals[i][0] && element < intervals[i][1]) {
+                    acc++;
+                }
             }
-        })
-        rawfi.push(acc)
+        });
+        rawfi.push(acc);
     }
-    return rawfi
-} 
-const fi = createFrequency(interval, dataMatrix) 
+    return rawfi;
+}
+
+
+
 
 function calculateFrequencies(frequency){
     let resultArray = []
@@ -106,8 +165,6 @@ function calculateFrequencies(frequency){
     }, 0);
     return resultArray
 }
-
-const Fi = calculateFrequencies(fi)
 
 function createRelativeFrequency(fi){
     let sum = fi.reduce((acc, current) => {
@@ -121,13 +178,7 @@ function createRelativeFrequency(fi){
     }
     return resultArray
 }
-const fr = createRelativeFrequency(fi)
-let sum = fr.reduce((acc, current) => {
-    return acc + current;
-});
 
-const Fr = calculateFrequencies(fr)
-let contentTable = [interval,fi,Fi,fr,Fr]
 
 function createtable(k,tableGrid,contentTable,sum){
     for(c=0;c<k+1;c++){
@@ -139,7 +190,7 @@ function createtable(k,tableGrid,contentTable,sum){
             
             
             switch (true) {
-                case (c === 5 && i === 3):
+                case (c === k && i === 3):
                     newCell.textContent = `${sum}%`;
                     newCell.style.backgroundColor = 'rgb(174, 205, 255)';
                     break;
@@ -163,11 +214,4 @@ function createtable(k,tableGrid,contentTable,sum){
     }
 }
 
-console.log(interval+" --intervals--")
-console.log(fi+" --fi--")
-console.log(Fi+" --Fi--")
-console.log(fr+" --fr--")
-console.log(Fr+" --Fr--")
-console.log(sum+" --sum--")
-console.log(contentTable)
 
